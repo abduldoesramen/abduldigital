495f257e606d3f92760daaf58e231dbf74c33a0b
abduldoesramen <abdulrehman821337@gmail.com>
2023-06-09 16:55:41 +1000

    Added basic HTML to represent navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">My website</a>
+    <div>
+      <div>
+        <a>My website</a>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
+      <div>
+        <ul>
           <li>
             <Link href="/">Home</Link>
           </li>
